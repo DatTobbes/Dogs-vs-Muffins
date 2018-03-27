83827c7f8056a07eb06620b06add7cce27bbebec
Tobias-PC\Tobias <buttermilchschoppen@gmx.de>
2018-03-27 16:01:48 +0200

    Dataset-Loader wurde überarbeitet
    Jupyter-Notebook erweitert

--- dataset.py
+++ dataset.py
@@ -37,7 +37,6 @@ class DataLoader:
                 imresize = cv2.resize(img, (img_Size_X, img_size_Y))
                 images.append(imresize)
                 labels.append(category)
-                #self.create_rnd_img(imresize, category, images, labels)
 
             category += 1
         print(len(labels))
@@ -48,10 +47,24 @@ class DataLoader:
         for index in range(len(images)):
             print(len(images))
             img = images[index]
-            flipt_img = self.augmenttator.rnd_flip(img)
-            gray_scaled_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-            cv2.imshow("", gray_scaled_img)
-            cv2.waitKey(0)
+            flipt_img = np.expand_dims(self.augmenttator.rnd_flip(img), axis=0)
+            #gray_scaled_img =  cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+            #img_gray =self.change_color(img)
+
+            images=np.vstack((images, flipt_img))
+            labels = np.append(labels, np.asarray(labels[index]))
+
+
+        return images, labels
+
+    def change_color(self, img):
+        r = img[:, :, 0]
+        g = img[:, :, 1]
+        b = img[:, :, 2]
+
+        color_converted = [b, r, g]
+        np.asarray(color_converted)
+        color_converted = np.reshape(color_converted, (171, 171, 3))
 
     def printImageAndLabelArray(self):
         print(images, labels)
@@ -70,49 +83,27 @@ class DataLoader:
         from sklearn.utils import shuffle
         return shuffle(img_array, label_array, random_state=9)
 
-    def create_rnd_img(self, img, label, images,labels):
-        new_img=img
-
-        flipt_img=self.augmenttator.rnd_flip(new_img)
-        images.append(flipt_img)
-        labels.append(label)
-        #cv2.imshow('flip', flipt_img)
-
-        # rot_img=self.augmenttator.rnd_rotation(new_img,[0,360])
-        # images.append(rot_img)
-        # labels.append(label)
-        #cv2.imshow('rot', rot_img)
-
-        # flipt_img = self.augmenttator.rnd_flip(new_img)
-        # rot_img = self.augmenttator.rnd_rotation(flipt_img, [0,360])
-        # images.append(rot_img)
-        # labels.append(label)
-        #cv2.imshow('image', rot_img)
-        #cv2.waitKey(0)
-        #cv2.destroyAllWindows()
-
 
 if __name__ == "__main__":
     data_dir = '.\\Images\\chihuahua-muffin\\train'
-    #testset= DataLoader()
-    #images, labels = testset.load_data(data_dir, 171,172)
-    #images, labels = testset.normalizeData(images, labels)
-    #testset.save_as_npz(images, labels, 'dog-vs-muffins.npz')
+    testset= DataLoader()
+    images, labels = testset.load_data(data_dir, 171,171)
+    testset.save_as_npz(images, labels, 'dogs-vs-muffins.npz')
     import matplotlib.pyplot as plt
 
-    #images, labels = testset.argument_images(images , labels)
-    testset= DataLoader()
-    dataset = np.load('test_data.npz')
-    labels, images = [dataset[f] for
-                      f in dataset.files]
+    dataset = np.load('dogs-vs-muffins.npz')
+    labels=dataset['labels']
+    images=dataset['images']
+    images, labels=testset.argument_images(images, labels)
+    images, labels = testset.normalizeData(images, labels)
     images_to_show = np.concatenate((images[labels == 0][:8], images[labels == 1][:8]), axis=0)
     print(len(images_to_show))
 
     fig = plt.figure(figsize=(10, 10))
     for i in range(16):
-        ax = fig.add_subplot(4, 4, 1 + i, xticks=[], yticks=[])
-        im = images_to_show[i]
-        plt.imshow(im)
+         ax = fig.add_subplot(8, 8, 1 + i, xticks=[], yticks=[])
+         im = images_to_show[i]
+         plt.imshow(im)
 
     plt.tight_layout()
     plt.show()
